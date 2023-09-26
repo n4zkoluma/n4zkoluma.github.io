40061048ad952bf21e86fe07b997e440d0b2270d
Nazar Kolomiiets <nkoluma@gmail.com>
2023-09-26 20:35:14 +0300

    crated Articles page, added scss file for it

---
+++ src/pages/Articles/Articles.js
@@ -0,0 +1,14 @@
+import Header from "../../components/Header/Header";
+import Footer from "../../components/Footer/Footer";
+
+const Articles = () => {
+  return(
+      <div>
+          <Header/>
+
+          <Footer/>
+      </div>
+  );
+}
+
+export default Articles;
