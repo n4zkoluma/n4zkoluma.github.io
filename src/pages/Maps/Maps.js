a59b9618770f074f972dd5a4cc18c04329928ca5
Nazar Kolomiiets <nkoluma@gmail.com>
2023-09-26 20:34:40 +0300

    crated Maps page, added scss file for it

---
+++ src/pages/Maps/Maps.js
@@ -0,0 +1,14 @@
+import Header from "../../components/Header/Header";
+import Footer from "../../components/Footer/Footer";
+
+const Maps = () => {
+  return(
+    <div>
+        <Header/>
+
+        <Footer/>
+    </div>
+  );
+}
+
+export default Maps;
